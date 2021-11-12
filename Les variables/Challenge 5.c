4666f0764b4c46a3d364dfb567972b3ceaac7b94
RIAD Abdelaziz <riadabdelaziz40@gmail.com>
2021-11-12 16:10:30 +0100

    c

--- Les variables/Challenge 5.c
+++ Les variables/Challenge 5.c
@@ -12,13 +12,16 @@ int main(){
 
 	if(c<0)
 	printf("Très froids %f",c);
+
 	else if (c<25)
 	printf("froid %f",c);
+
 	else if(c<37)
 	printf("chaud %f",c);
+
 	else
 	printf("très chaud %f",c);
-	printf("voila la temperature en c %f",c);
+	
 
 	return 0;
 
