d324754b78a9ddb9fbeb2abb2e32008b320ea4bd
RIAD Abdelaziz <riadabdelaziz40@gmail.com>
2021-11-15 08:57:56 +0100

    c

--- Les variables/Challenge 8.c
+++ Les variables/Challenge 8.c
@@ -3,31 +3,31 @@
 
 int main()
 {
-    int a;
-    int b;
-    int c;
-    int d;
-    int s;
-    int m;
+    float a;
+    float b;
+    float c;
+    float d;
+    float s;
+    float m;
     
     printf("Tapez le nombre a \n");
-    scanf("%d", &a);
+    scanf("%f", &a);
     
     printf("Tapez le nombre b \n");
-    scanf("%d", &b);
+    scanf("%f", &b);
     
     printf("Tapez le nombre c \n");
-    scanf("%d", &c); 
+    scanf("%f", &c); 
     
     printf("Tapez le nombre d \n");
-    scanf("%d", &d);
+    scanf("%f", &d);
    
     s = a + b + c + d;
-    printf("la somme est s=  %d\n", s);
+    printf("la somme est s=  %f\n", s);
     
     m = s / 4 ;
     
-    printf("le moyenne est s/2=  %d\n", m);
+    printf("le moyenne est s/4 =  %f\n", m);
    
     return 0;
 }
