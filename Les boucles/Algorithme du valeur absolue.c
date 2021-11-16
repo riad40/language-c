ee05512fa9b0ba0e1c715f11c65a3e308b91b4f9
RIAD Abdelaziz <riadabdelaziz40@gmail.com>
2021-11-16 13:42:36 +0100

    va

---
+++ Les boucles/Algorithme du valeur absolue.c
@@ -0,0 +1,15 @@
+Algorithme / affichage du valeur absolue /
+
+variable va : entier
+
+Debut
+
+ecrire('entrez un nombre entier (positif ou negetif)')
+lire (nbr)
+
+si va < 0 alors va<-va*(-1)
+ecrire('la valeur absolue est VA')
+
+finsi
+    fin
+
